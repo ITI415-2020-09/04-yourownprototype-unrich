97ed5ba55dcc275a553f3a83e3becc7144b34c31
unrich <60159029+unrich@users.noreply.github.com>
2020-10-02 01:52:27 -0400

    Basic of Game
    Just like I did in mission demolition, I want to create a slingshot that can fires projectiles to Pickups which are in the sky. Pickups can rotate. More than that, I want my Slingshot can move just like player in Roll-a-ball. I already finished the slingshot, the ground, pickups, and projectile. However, there are some problems with my scripts. Right now, my slingshot cannot move, and after I added code of movement into my Slingshot script, the launchpoint of Slingshot was missed.

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject PickUpPrefabs;
+    public static System.Random rnd = new System.Random();
+
+        void Start()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            int x = rnd.Next(-8, 8);
+            int y = rnd.Next(2, 9);
+            Instantiate(PickUpPrefabs, new Vector3(x, y, 0),  Quaternion.identity);
+            Debug.Log("aaaaaa");
+        }   
+    }
+}
---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject prefabsProjectile;
+    public float speed = 0;
+    public float velocityMult = 4f;
+    public bool _____________________________;
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+    static public Slingshot S;
+    private float movementX;
+    private float movementY;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+    private void Awake()
+    {
+        S = this;
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    }
+
+    void OnMouseEnter()
+    {
+        launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(prefabsProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!aimingMode) return;
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+            projectile.GetComponent<Rigidbody>().isKinematic = false;
+            projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
+            projectile = null;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 movment = new Vector2(movementX, movementY);
+        rb.AddForce(movment * speed);
+    }
+}
+
